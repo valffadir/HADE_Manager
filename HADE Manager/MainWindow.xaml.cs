a473c6e7336e207ba5267a3b73b4df4006c35c64
valffadir <alejandro.quiroz.f@gmail.com>
2017-06-10 05:48:55 -0500

    Final MainWindow

--- HADE Manager/MainWindow.xaml.cs
+++ HADE Manager/MainWindow.xaml.cs
@@ -25,7 +25,17 @@ namespace HADE_Manager
     {
         public MainWindow()
         {
-            InitializeComponent();
+            DataContext = new MainWindowViewModel();
+
+            Task.Factory.StartNew(() =>
+            {
+                Thread.Sleep(2500);
+            }).ContinueWith(t =>
+            {
+                //note you can use the message queue from any thread, but just for the demo here we 
+                //need to get the message queue from the snackbar, so need to be on the dispatcher
+                MainSnackbar.MessageQueue.Enqueue("Bienvenido");
+            }, TaskScheduler.FromCurrentSynchronizationContext());
         }
 
         private void UIElement_OnPreviewMouseLeftButtonUp(object sender, MouseButtonEventArgs e)
---
+++ HADE Manager/MainWindowViewModel.cs
@@ -0,0 +1,54 @@
+﻿using HADE_Manager;
+using MaterialDesignThemes.Wpf;
+using MaterialDesignThemes.Wpf.Transitions;
+using System.Windows.Controls;
+
+namespace HADE_Manager
+{
+    public class MainWindowViewModel
+    {
+        public MainWindowViewModel()
+        {
+            //MenuItems = new[]
+            //{
+                //new MenuItem("Home", new Home()
+                //    ),
+                //new MenuItem("Palette", new PaletteSelector { DataContext = new PaletteSelectorViewModel() } 
+                //    ),
+                //new MenuItem("Buttons & Toggles", new Buttons())
+                //    {
+                //        VerticalScrollBarVisibilityRequirement = ScrollBarVisibility.Auto
+                //    },
+                //new MenuItem("Fields", new TextFields())
+                //    {
+                //        VerticalScrollBarVisibilityRequirement = ScrollBarVisibility.Auto
+                //    },
+                //new MenuItem("Pickers", new Pickers { DataContext = new PickersViewModel()}
+                //    ),
+                //new MenuItem("Sliders", new Sliders()
+                //    ),
+                //new MenuItem("Chips", new Chips()
+                //    ),
+                //new MenuItem("Typography", new Typography())
+                //    {
+                //        VerticalScrollBarVisibilityRequirement = ScrollBarVisibility.Auto,
+                //        HorizontalScrollBarVisibilityRequirement = ScrollBarVisibility.Auto
+                //    },
+                //new MenuItem("Cards", new Cards())
+                //{
+                //    VerticalScrollBarVisibilityRequirement = ScrollBarVisibility.Auto
+                //},
+                //new MenuItem("Icon Pack", new IconPack { DataContext = new IconPackViewModel() }
+                //    ),
+                //new MenuItem("Colour Zones", new ColorZones()
+                //    ),
+                //new MenuItem("Lists", new Lists { DataContext = new ListsAndGridsViewModel()})
+                //    {
+                //        VerticalScrollBarVisibilityRequirement = ScrollBarVisibility.Auto   
+                //    },  
+            //};
+        }
+
+        public MenuItem[] MenuItems { get; }
+    }
+}
